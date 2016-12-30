9203fc3d03b0a0ca94b7520e6c6c8dcf478dc2fd
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-12-29 23:26:52 -0700

    cleaned up 404

--- 404.php
+++ 404.php
@@ -19,9 +19,6 @@ get_header(); ?>
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links above?', 'kamstrafam' ); ?></p>
-
-					
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 			</div>
